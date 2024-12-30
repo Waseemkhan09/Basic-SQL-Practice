88770ca532658cb3ff55e7b3a1393cf8f8cf8089
Waseemkhan09 <162962026+Waseemkhan09@users.noreply.github.com>
2024-12-30 19:53:34 +0530

    Create 13.TOP_N.sql

---
+++ 13.TOP_N.sql
@@ -0,0 +1,16 @@
+--Hi we are learning SQL
+
+/* We
+are learning
+sql
+*/
+
+select * from [dbo].[EmployeeRecords]
+
+select top 2 * from [dbo].[EmployeeRecords]
+
+select top 5 * from [dbo].[EmployeeRecords]
+
+select top 2  EmployeeID,Salary from [dbo].[EmployeeRecords]
+
+select top 4  FirstName,LastName from [dbo].[EmployeeRecords]
